144be89a2fe60126b41167fd00b0e49fabc44ef9
HL <lecaoquochung@gmail.com>
2016-09-22 20:28:02 +0900

    Sample module Album route MVC

--- sample/module/Album/config/module.config.php
+++ sample/module/Album/config/module.config.php
@@ -1,9 +1,29 @@
 <?php
 namespace Album;
 
+use Zend\Router\Http\Literal;
+use Zend\Router\Http\Segment;
 use Zend\ServiceManager\Factory\InvokableFactory;
 
 return [
+    'router' => [
+        'routes' => [
+            'album' => [
+                'type'    => Segment::class,
+                'options' => [
+                    'route' => '/album[/:action[/:id]]',
+                    'constraints' => [
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ],
+                    'defaults' => [
+                        'controller' => Controller\AlbumController::class,
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+        ],
+    ],
     'controllers' => [
         'factories' => [
             Controller\AlbumController::class => InvokableFactory::class,
